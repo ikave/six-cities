b1ead47a94307c66e8065c4b83d87c2b5c8d2fb0
ikave <shiftgear@mail.ru>
2022-08-02 18:20:24 +0300

    Добавляет обработчик по наведению мыши

--- project/src/pages/room/room.tsx
+++ project/src/pages/room/room.tsx
@@ -180,6 +180,7 @@ export const Room = () => {
               offersList={nearby}
               cardClasses={CARD_CLASSES.near}
               activeCardId={activeCardId}
+              onMouseOver={setActiveCardId}
             />
           </section>
         </div>
