f13ca4e2f52ba03d9b374942f83c005431406e24
ikave <shiftgear@mail.ru>
2022-08-12 12:31:09 +0300

    Подключает данные о предложениях с сервера

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,6 @@
 import { Router } from '../router';
-import { useAppDispatch } from '../../hooks';
-import { getAllOffers } from '../../store/action';
-import { offers } from '../../mocks/offers';
 
 function App() {
-  const dispatch = useAppDispatch();
-
-  dispatch(getAllOffers({ offers }));
-
   return <Router />;
 }
 
